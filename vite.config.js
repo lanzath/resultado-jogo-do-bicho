1ae1054e1098306183dd95a501f8c8704b7b0980
Thiago Lanza <lanzathm@gmail.com>
2024-06-10 21:05:48 -0300

    Adjusts

--- vite.config.js
+++ vite.config.js
@@ -2,43 +2,51 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react-swc'
 
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name: 'Jogo do Bicho - Resultado',
+    short_name: 'Jogo do Bicho - Resultado',
+    description: 'Visualizar resultado do jogo do bicho Deu no Poste',
+    icons:[{
+      src: '/android-chrome-192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'/android-chrome-512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'prompt',
-      includeAssets: ['react.svg'],
-      manifest: {
-        name: 'Jogo do Bicho - Resultado',
-        short_name: 'Jogo do Bicho - Resultado',
-        description: 'Visualizar resultado do jogo do bicho Deu no Poste',
-        icons: [
-          {
-            src: '/android-chrome-dog-256x256.png',
-            sizes: '256',
-            type: 'image/png'
-          },
-          {
-            src: '/android-chrome-dog-512x512.png',
-            sizes: '512',
-            type: 'image/png'
-          },
-          {
-            src: '/android-chrome-dog-256x256.png',
-            sizes: '256',
-            type: 'image/png',
-            purpose: 'any maskable'
-          },
-        ],
-        theme_color: '#171717',
-        background_color: '#f3f3f3',
-        scope: '/',
-        display: 'standalone',
-        start_url: '/',
-        orientation: 'portrait'
-      }
-    }
-    )],
+    VitePWA(manifestForPlugIn)
+  ]
 })
