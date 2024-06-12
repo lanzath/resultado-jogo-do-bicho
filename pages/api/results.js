38e5ac3187cbc1f1d37932f8098bd5e73d618bef
Thiago Lanza <lanzathm@gmail.com>
2024-06-11 22:36:08 -0300

    Hotfix

--- pages/api/results.js
+++ pages/api/results.js
@@ -47,6 +47,6 @@ export default function handler(req, res) {
       res.status(200).json(results);
     })
     .catch(err => {
-      res.status(err.status).json(err);
+      res.status(400).json(err);
     });
 }
